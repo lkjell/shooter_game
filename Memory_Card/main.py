d1acbdd938409ee79cd339068fd3e009534fafa6
Kjell Le <lkjell@gmail.com>
2022-06-18 11:01:32 +0200

    Add show result and question functions.

--- Memory_Card/main.py
+++ Memory_Card/main.py
@@ -1,12 +1,41 @@
+from random import shuffle
 from PyQt5.QtCore import Qt
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton,
-                             QMessageBox, QGroupBox)
+                             QMessageBox, QGroupBox, QButtonGroup)
 
 
-def answer_button_action():
+def show_result():
     RadioGroupBox.hide()
     AnsGroupBox.show()
+    answer_button.setText("Next question")
+
+
+def show_questions():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    answer_button.setText("Answer")
+
+    RadioGroup.setExclusive(False)
+    button1.setChecked(False)
+    button2.setChecked(False)
+    button3.setChecked(False)
+    button4.setChecked(False)
+    RadioGroup.setExclusive(True)
+
+
+def ask(question, right_answer, wrong1, wrong2, wrong3):
+    shuffle(answer)
+    answer[0].setText(right_answer)
+    answer[1].setText(wrong1)
+    answer[2].setText(wrong2)
+    answer[3].setText(wrong3)
+
+def start_test():
+    if answer_button.text() == "Answer":
+        show_result()
+    else:
+        show_questions()
 
 
 app = QApplication([])
@@ -27,6 +56,15 @@ button1 = QRadioButton("Enets")
 button2 = QRadioButton("Smufts")
 button3 = QRadioButton("Chulyms")
 button4 = QRadioButton("Aleuts")
+answers = [button1, button2, button3, button4]
+
+RadioGroup = QButtonGroup()
+for btn in answers:
+    RadioGroup.addButton(btn)
+# RadioGroup.addButton(button1)
+# RadioGroup.addButton(button2)
+# RadioGroup.addButton(button3)
+# RadioGroup.addButton(button4)
 
 AnsGroupBox = QGroupBox("Test result")
 ans_result = QLabel("True/False")
@@ -34,7 +72,7 @@ ans_correct = QLabel("Correct answer")
 AnsGroupBox.hide()
 
 answer_button = QPushButton("Answer")
-answer_button.clicked.connect(answer_button_action)
+answer_button.clicked.connect(start_test)
 
 # Layout
 
@@ -63,13 +101,20 @@ layoutv_ans.addWidget(ans_correct, alignment=Qt.AlignCenter)
 
 AnsGroupBox.setLayout(layoutv_ans)
 
+# Layout answer button
+layouth_ans_button = QHBoxLayout()
+layouth_ans_button.addStretch(2)
+layouth_ans_button.addWidget(answer_button, 1)
+layouth_ans_button.addStretch(2)
+
 # add widgets to main layout
 layout_main = QVBoxLayout()
 layout_main.addWidget(question, alignment=Qt.AlignCenter)
 layout_main.addWidget(RadioGroupBox)
 layout_main.addWidget(AnsGroupBox)
-layout_main.addWidget(answer_button, stretch=0)
-layout_main.setSpacing(0)
+layout_main.addLayout(layouth_ans_button)
+# layout_main.addWidget(answer_button, stretch=0)
+# layout_main.setSpacing(0)
 
 my_win.setLayout(layout_main)
 
